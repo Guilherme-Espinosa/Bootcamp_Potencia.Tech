6944730334e35a40dd399d9d419129cc7cadfe69
Guilherme-Espinosa <luiz5913@live.com>
2023-05-25 05:48:02 -0300

    code challenge 3 and 4 ok

---
+++ trilha3/desafio3/codeChallenge3.js
@@ -0,0 +1,13 @@
+const valorPedido = parseInt(gets());
+
+//TODO: Criar as condições necessárias para impressão da saída conforme o enunciado.
+
+
+function (valorPedido){
+  if (valorPedido => 50.00){
+  print 'Parabens, você ganhou uma sobremesa gratis!'
+} else {
+  print 'Que pena, você nao ganhou nenhum brinde especial.'
+}
+
+// Desafio 03 ok!
---
+++ trilha3/desafio4/codeChallenge4.js
@@ -0,0 +1,29 @@
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
+
+const numPedidos = parseInt(gets());
+
+for (let i = 1; i <= numPedidos; i++) {
+  const prato = gets();
+  const calorias = parseInt(gets());
+  //Explicação: atribui "true" se o valor lido via gets() for estritamente igual a 's'.
+  //Referência: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_operators
+  const ehVegano = gets().toLowerCase() === 's';
+
+  //TODO: Tendo em vista a variável booleana "ehVegano", imprima a saída deste desafio.
+
+const vegano = ehVegano ? 'Vegano' : 'Nao-vegano';
+
+// Saída
+
+print(`Pedido ${i}: ${prato} (${vegano}) - ${calorias} calorias`);
+}
+
+
+
+
+
+
+
+
